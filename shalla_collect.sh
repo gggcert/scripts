38f6a7bc1776c9c0e308e26e770b01b75b6ca0ad
gggcert <31406381+gggcert@users.noreply.github.com>
2017-09-28 17:59:35 +0300

    Update shalla_collect.sh
    create helper stub for the named.conf on both dns and rpz servers with new zones
    cp the ready zone files into production

--- shalla_collect.sh
+++ shalla_collect.sh
@@ -2,8 +2,10 @@
 #
 # shalla_collect.sh
 # OWNER: Gilad Finkelstein
-# VER:   0.4 20170924
+# VER:   0.5 20170928
 # 
+# 0.5 	create helper stub for the named.conf on both dns and rpz servers with new zones
+#		cp the ready zone files into production
 # Collect sbl lists from feed shalla
 #----------------------------------------------------------------------
 # retrive file only:
@@ -101,7 +103,7 @@ if [ -e $workdir/$feedFile ]; then
     $md5Cmd --status -c $feedFile.md5
 
     #if downloaded md5 matches the local file we do not need an update
-    [ $? -eq 0 ] && needNewVersion=0
+    [ $? -eq 0 ] && needNewVersion=0 && echo "Nothing to do $feedFile is up to date" #&& exit 2 
 fi
  #we know we need to update the black list collection file lets get it
 if [ $needNewVersion -eq 1 ]; then
@@ -110,7 +112,7 @@ if [ $needNewVersion -eq 1 ]; then
 	#clean up old BL directories 
 	[ -d $feedworkingdir ] && echo "Old blacklist directory found in $feedworkingdir. Deleted!" && rm -rf $feedworkingdir
 	#fetch bl file
-	echo "Feth fresh BL file from $feedName,stand by"
+	echo "Fetch fresh BL file from $feedName,stand by"
 	$httpget $feedUrl -a $feedLog -O $workdir/$feedFile || { echo "Unable to download $feedFile." && exit 1 ; }
     #Check the status
     $md5Cmd --status -c $feedFile.md5
@@ -220,43 +222,77 @@ for cat in $CATEGORIES ;do
 	
 done
 #set -x
-for zf in $(ls $zoneDir) ;do
+validZoneNames=''
+for zf in $(ls $zoneDir|grep -v '.conf$') ;do
 	echo "checking zone $zoneDir/$zf"
 	#go over all created zone files and validate zone file integrity 
 	zn=$(grep '^$ORIGIN' $zoneDir/$zf|awk '{print $2}')
 	named-checkzone $zn $zoneDir/$zf
 	# if check fails move it aside and do not attempt to load it in the next step 
-	[ $? -ne 0 ]  && echo "something is bad with the zone please check it manually,file moved to $zoneDir/$zf.bad" &&  mv $zoneDir/$zf $zoneDir/$zf.bad
+	if [ $? -ne 0 ]  ;then 
+		echo "something is bad with the zone please check it manually,file moved to $zoneDir/$zf.bad"  
+		mv $zoneDir/$zf $zoneDir/$zf.bad
+	else
+		validZoneNames+=" $zn"
+	fi
 done
 echo "Processing the feed had finished all valid zone files can be found in $zoneDir/$zf"
-echo "stub named.conf changes can be found in $zoneDir/stub_missing_zpnes.conf"
+
+
+#helper code to generate stubs for the dns and rpz named.conf changes
+#The dns servers map is only required for helping creat the correct required stub named.conf file for bind users to use. This is Optional only 
+declare -A DNS_SERVERS=([cert]=172.17.1.122 [certDev]=172.17.1.122)
+
+myIp=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'|head -1)
+#helper conf files to help with configuring both the rpz and the dns itself 
+bindConf=$zoneDir/stub_bindConf.conf && rm -rf $bindConf
+rpzConf=$zoneDir/stub_rpzConf.conf && rm -rf $rpzConf
+#filetr out invalid zone files
+for zf in $(ls $zoneDir|grep -v '.bad$'| grep -v '.conf$') ;do
+	if [ ! -e $rpzConf ] ;then
+		#create the stub headers of the conf files
+		echo '//zone name convention is "subscriber_internalFeedName_category" e.g. cert.kuku1.other => cert subscriber for kuku1 feed for unspcificed category (other)' |tee -a $bindConf $rpzConf >/dev/null
+		echo 'options {'|tee -a $bindConf $rpzConf >/dev/null		
+		echo '...' |tee -a $bindConf $rpzConf >/dev/null
+		#e.g. response-policy {zone kuku1_other_rpz.db; zone kuku1_spyware_rpz.db; zone kuku1_spyware_rpz.db.bad2; };
+		echo '    response-policy {'$(echo $validZoneNames|tr ' ' '\n'|sed 's/^/zone /g'|sed 's/$/;/g'|xargs)'};' |tee -a $bindConf $rpzConf >/dev/null
+		echo '...' |tee -a $bindConf $rpzConf >/dev/null
+		echo '}' |tee -a $bindConf $rpzConf >/dev/null
+	fi
+	#create the stub example for help ${DNS_SERVERS[$subscriber]} for bind server
+	zn=$(grep '^$ORIGIN' $zoneDir/$zf|awk '{print $2}')
+	echo	'zone "'$zn'"{' |tee -a $bindConf $rpzConf >/dev/null
+	#generate the stub for the dns server 
+	echo	'    type slave;' >> $bindConf
+	echo	'    masters {' $myIp '; };' >> $bindConf
+	echo	'    file "slaves/'${subscriber}_${zf}'";' >> $bindConf
+	echo	'    allow-query { localhost; };' >> $bindConf
+	echo	'    allow-transfer { none; };' >> $bindConf
+	echo	'};' >> $bindConf
+	#generate the stub for the rpz server 
+	echo	'    type master; '>> $rpzConf
+	echo	'    file "data/'${subscriber}_${zf}'";' >> $rpzConf
+	echo	'    also-notify { '${DNS_SERVERS[$subscriber]}';};' >> $rpzConf
+	echo	'    notify yes; '>> $rpzConf
+	echo	'    allow-transfer { '${DNS_SERVERS[$subscriber]}';};' >> $rpzConf
+	echo	'}; '>> $rpzConf
+	
+	echo 	''|tee -a $bindConf $rpzConf >/dev/null
+	#cp the zone file in place
+	echo "Coping new created valid zone file [ $zf] into production [$bindRootDir/data/]"
+	#set -x
+	cp -rf $zoneDir/$zf $bindRootDir/data/${subscriber}_${zf}
+	#set +x
+done
+echo "stub named.conf changes can be found in $bindConf $rpzConf"
 # while we can run the collection and parsing on different server it would make sense to run it directly on the rpz dns, make sure we have it running
 [ ! -d $bindRootDir ] && echo "Are you sure we are running on the rpz named(bind) dns server? I failed to find $bindRootDir" && exit 1
 # check if new zone exists in /etc/named.conf if not add it to the stub (so someone can manually add it after a review)
 # if it does exists cpy the zone to /var/named/data/ 
-#zone "cert.kuku1.other"{
-#  type master;
-#  file "data/cert_kuku1_other_rpz.db";
-#	also-notify { 172.17.1.122;};
-#	allow-transfer { 172.17.1.122;};
-#	notify yes;
-#};
 
-#for each domain file use a template to create required zone file 
-#increase the serial number of that zone file by 1 
-#place it in the dns-rpz zone directory and reload dnz-rpz (triggering a notification to all subscribers)
-# echo "Setting file permisions."
-# $chownpath -R $squidGuardowner $dbhome
-# chmod 755 $dbhome
-# cd $dbhome
-# find . -type f -exec chmod 644 {} \;
-# find . -type d -exec chmod 755 {} \;
+echo "Zone files updated and should be already synced with DNS server [ check the log in /var/named/data/named.log]"
+tail  /var/named/data/named.log
 
-# echo "Updating squid db files with diffs."
-# $squidGuardpath -u all
-
-# echo "Reconfiguring squid."
-# $squidpath -k reconfigure
 
 
 exit 0
